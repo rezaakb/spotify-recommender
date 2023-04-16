133bcefb9b7e44d7fb7b200d5c66212d66fd39d1
Reza Akbarian Bafghi <r.akbarian98@yahoo.com>
2023-04-15 20:06:13 -0600

    Update deploy-local-dev.sh

--- deploy-local-dev.sh
+++ deploy-local-dev.sh
@@ -11,9 +11,9 @@
 kubectl apply -f ./redis/redis-deployment.yaml
 kubectl apply -f ./redis/redis-service.yaml
 
-#kubectl apply -f ./rest/rest-deployment.yaml
-#kubectl apply -f ./rest/rest-service.yaml
+kubectl apply -f ./rest/rest-deployment.yaml
+kubectl apply -f ./rest/rest-service.yaml
 kubectl apply -f ./logs/logs-deployment.yaml
-#kubectl apply -f ./worker/worker-deployment.yaml
+kubectl apply -f ./worker/worker-deployment.yaml
 
 kubectl port-forward --address 0.0.0.0 service/redis 6379:6379 &
